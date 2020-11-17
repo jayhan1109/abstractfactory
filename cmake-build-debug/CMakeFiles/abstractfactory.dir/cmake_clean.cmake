a8303db989e8db92f1c6c8f230e76b51a6769242
jayhan1109 <jayhan1109@gmail.com>
2020-11-18 04:23:37 +0900

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/abstractfactory.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/abstractfactory.dir/main.cpp.obj"
+  "abstractfactory.exe"
+  "abstractfactory.exe.manifest"
+  "abstractfactory.pdb"
+  "libabstractfactory.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/abstractfactory.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
